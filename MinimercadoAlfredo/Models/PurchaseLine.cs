59ad4bd086bb486ee2ba51b07deda5eb85048aeb
mln6666 <poncemarioleo@gmail.com>
2016-10-20 09:38:48 -0300

    subite la puta que te pario

---
+++ MinimercadoAlfredo/Models/PurchaseLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MinimercadoAlfredo.Models
+{
+    public class PurchaseLine
+    {
+        [Key]
+        public int IdPurchaseLine { get; set; }
+
+        public decimal PurchaseQuantity { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public decimal LineTotal { get; set; }
+
+        public int IdPurchase { get; set; } //Clave Foránea de Purchase
+
+        public virtual Purchase Purchase { get; set; }
+
+        public int IdProduct { get; set; } //Clave Foránea de Producto
+
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ MinimercadoAlfredo/ViewModels/PurchaseVM.cs
@@ -0,0 +1,21 @@
+﻿using MinimercadoAlfredo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinimercadoAlfredo.ViewModels
+{
+    public class PurchaseVM
+    {
+        public Provider Provider { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public List<SaleLine> PurchaseLines { get; set; }
+
+        public float PurchaseTotal { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
